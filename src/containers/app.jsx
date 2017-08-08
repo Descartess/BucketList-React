f66d46bb8b2272c0723f1e2d89fd724a44c3a4d2
NyondoPaul <nyondopaul23@gmail.com>
2017-08-08 13:19:25 +0300

    Refactor components

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,69 +1,16 @@
 import React, { Component } from 'react';
-import Banner from '../components/banner';
 
-import SignInForm from '../components/signinform';
-import SignUpForm from '../components/signupform';
+import Authenticate from './authenticate';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      showSignIn: false,
-      showSignUp: false,
-      username: '',
-      password: '',
-      rpassword: '',
-
-    };
-    this.onSignInShow = this.onSignInShow.bind(this);
-    this.onSignUpShow = this.onSignUpShow.bind(this);
-    this.handleUsernameInput = this.handleUsernameInput.bind(this);
-    this.handleRepeatPasswordInput = this.handleRepeatPasswordInput.bind(this);
-    this.handlePasswordInput = this.handlePasswordInput.bind(this);
+    this.state = { authenticated: false };
   }
-  onSignInShow() {
-    this.setState({ showSignIn: !this.state.showSignIn });
-  }
-
-  onSignUpShow() {
-    this.setState({ showSignUp: !this.state.showSignUp });
-  }
-
-  handleUsernameInput(e) {
-    this.setState({ username: e.target.value });
-  }
-
-  handlePasswordInput(e) {
-    this.setState({ password: e.target.value });
-  }
-
-  handleRepeatPasswordInput(e) {
-    this.setState({ rpassword: e.target.value });
-  }
-
   render() {
-    console.log(this.state);
     return (
       <div>
-        <Banner
-          onSignIn={this.onSignInShow}
-          onSignUp={this.onSignUpShow}
-        />
-        <SignInForm
-          data={this.state}
-          show={this.state.showSignIn}
-          onSignIn={this.onSignInShow}
-          userChange={this.handleUsernameInput}
-          passwordChange={this.handlePasswordInput}
-        />
-        <SignUpForm
-          data={this.state}
-          show={this.state.showSignUp}
-          onSignUp={this.onSignUpShow}
-          userChange={this.handleUsernameInput}
-          passwordChange={this.handlePasswordInput}
-          rpasswordChange={this.handleRepeatPasswordInput}
-        />
+        <Authenticate />
       </div>
     );
   }
---
+++ src/containers/authenticate.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { registerUser, signinUser } from '../actions';
+import Banner from '../components/banner';
+
+import SignInForm from '../components/signinform';
+import SignUpForm from '../components/signupform';
+
+class Authenticate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSignIn: false,
+      showSignUp: false,
+      username: '',
+      password: '',
+      rpassword: '',
+
+    };
+    this.onSignInShow = this.onSignInShow.bind(this);
+    this.onSignUpShow = this.onSignUpShow.bind(this);
+    this.handleUsernameInput = this.handleUsernameInput.bind(this);
+    this.handleRepeatPasswordInput = this.handleRepeatPasswordInput.bind(this);
+    this.handlePasswordInput = this.handlePasswordInput.bind(this);
+    this.onSignIn = this.onSignIn.bind(this);
+    this.onSignUp = this.onSignUp.bind(this);
+  }
+  onSignInShow() {
+    this.setState({ showSignIn: !this.state.showSignIn });
+  }
+
+  onSignIn() {
+    const { username, password } = this.state;
+    this.props.signinUser({ username, password });
+  }
+
+  onSignUp() {
+    const { username, password } = this.state;
+    this.props.registerUser({ username, password });
+  }
+
+  onSignUpShow() {
+    this.setState({ showSignUp: !this.state.showSignUp });
+  }
+
+  handleUsernameInput(e) {
+    this.setState({ username: e.target.value });
+  }
+
+  handlePasswordInput(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  handleRepeatPasswordInput(e) {
+    this.setState({ rpassword: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Banner
+          onSignInShow={this.onSignInShow}
+          onSignUpShow={this.onSignUpShow}
+        />
+        <SignInForm
+          data={this.state}
+          show={this.state.showSignIn}
+          onSignIn={this.onSignIn}
+          onSignInShow={this.onSignInShow}
+          userChange={this.handleUsernameInput}
+          passwordChange={this.handlePasswordInput}
+        />
+        <SignUpForm
+          data={this.state}
+          show={this.state.showSignUp}
+          onSignUp={this.onSignUp}
+          onSignUpShow={this.onSignUpShow}
+          userChange={this.handleUsernameInput}
+          passwordChange={this.handlePasswordInput}
+          rpasswordChange={this.handleRepeatPasswordInput}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { loading } = state.auth;
+  return { loading };
+};
+
+Authenticate.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  registerUser: PropTypes.func.isRequired,
+  signinUser: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, { registerUser, signinUser })(Authenticate);
