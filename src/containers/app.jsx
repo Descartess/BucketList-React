315ab41820d3d3303fe22c6e7e7180ce4ec8fb51
NyondoPaul <nyondopaul23@gmail.com>
2017-08-10 13:19:06 +0300

    Add Redux and display logic

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import Authenticate from './authenticate';
+import Home from './home';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { authenticated: false };
   }
+  renderScreen() {
+    const { authenticated, token } = this.props;
+    if (authenticated && token) {
+      return (<Home />);
+    }
+    return (<Authenticate />);
+  }
   render() {
     return (
       <div>
-        <Authenticate />
+       { this.renderScreen() }
       </div>
     );
   }
 }
+const mapStateToProps = (state) => {
+  const { authenticated, token } = state.auth;
+  return { authenticated, token };
+};
+export default connect(mapStateToProps)(App);
 
-export default App;
+export { App }
