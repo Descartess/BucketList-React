03c0ae29ee3d4654412c8d99291f0f8fd023febd
NyondoPaul <nyondopaul23@gmail.com>
2017-08-10 12:59:02 +0300

    Add Auth promises as payload

--- src/actions/authactions.js
+++ src/actions/authactions.js
@@ -2,10 +2,10 @@ import instance from '../config/axiosconfig';
 
 export const registerUser = ({ username, password }) => ({
   type: 'REGISTER',
-  payload: instance.post('auth/register', { username, password }),
+  payload: instance.post('/auth/register', { username, password }),
 });
 
 export const signinUser = ({ username, password }) => ({
   type: 'SIGNIN',
-  payload: instance.post('auth/login', { username, password }),
+  payload: instance.post('/auth/login', { username, password }),
 });
