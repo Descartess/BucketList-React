0fc48c8736d8058f9b400517aaf271dbd25f67f6
NyondoPaul <nyondopaul23@gmail.com>
2017-08-10 12:53:00 +0300

    Add Interceptors

--- src/config/axiosconfig.js
+++ src/config/axiosconfig.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
+import store from './store';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'http://localhost:5000',
 });
 
 instance.defaults.timeout = 5000;
+instance.interceptors.request.use((config) => {
+  const token = store.getState().auth.token;
+  if (token) {
+    config.headers.authorization = `Bearer ${token}`; // eslint-disable-line no-param-reassign
+  }
+  return config;
+});
 
 export default instance;
