2a220819f66f2571ea44e2e97913a905c75b9e0d
NyondoPaul <nyondopaul23@gmail.com>
2017-08-10 13:11:01 +0300

    Add Register, Signin Reducers

--- src/reducers/authreducer.js
+++ src/reducers/authreducer.js
@@ -1,18 +1,33 @@
 const INITIAL_STATE = {
   loading: false,
   token: null,
+  authenticated: false,
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'REGISTER_PENDING':
       return { ...state, loading: true };
+    case 'REGISTER_REJECTED':
+      return { ...state, loading: false, error: true };
     case 'REGISTER_FULFILLED':
-      return { ...state, loading: false, token: action.payload.data.token };
+      return {
+        ...state,
+        loading: false,
+        token: action.payload.data.token,
+        authenticated: true,
+      };
     case 'SIGNIN_PENDING':
       return { ...state, loading: true };
+    case 'SIGNIN_REJECTED':
+      return { ...state, loading: true, error: true };
     case 'SIGNIN_FULFILLED':
-      return { ...state, loading: false, token: action.payload.data.token };
+      return {
+        ...state,
+        loading: false,
+        token: action.payload.data.token,
+        authenticated: true,
+      };
     default:
       return state;
   }
