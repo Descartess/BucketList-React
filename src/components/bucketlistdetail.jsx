f8a094f039f4e6811149c45803e09d1aaded8b76
Descartess <nyondopaul23@gmail.com>
2017-08-19 11:31:54 +0300

    Refactor bucketlist detail

--- src/components/bucketlistdetail.jsx
+++ src/components/bucketlistdetail.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { ListGroup, ListGroupItem, Glyphicon } from 'react-bootstrap';
+import { ListGroupItem, Glyphicon } from 'react-bootstrap';
 
-const renderDetail = bucketlists => bucketlists.map(bucketlist => (
-  <div key={bucketlist.id}>
-    <ListGroupItem>
-      {bucketlist.name}
-      <div className="pull-right">
-        <Glyphicon glyph="pencil" onClick={() => console.log('edit')} />
-        <Glyphicon glyph="trash" onClick={() => console.log('delete')} />
-      </div>
-    </ListGroupItem>
-  </div>
-    ));
-
-const BucketListDetail = (props) => {
-  const { data } = props;
-  return (
-    <ListGroup>
-      { renderDetail(data) }
-    </ListGroup>
-  );
-};
+const BucketListDetail = props => (
+  <ListGroupItem>
+    {props.name}
+    <div className="pull-right">
+      <Glyphicon glyph="pencil" onClick={props.edit} />
+      <Glyphicon glyph="trash" onClick={props.delete} />
+    </div>
+  </ListGroupItem>
+);
 
 export default BucketListDetail;
