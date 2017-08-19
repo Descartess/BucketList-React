c5aed35abb749601a28c1b51ae5f3f32aa88066a
Descartess <nyondopaul23@gmail.com>
2017-08-19 11:32:21 +0300

    Add confitm delete component

---
+++ src/components/confirm_delete.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+const ConfirmDelete = props => (
+  <Modal
+    show={props.show}
+    bsSize="small"
+  >
+    <Modal.Header>
+      <Modal.Title> Delete {props.name} </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <p>Are you sure you want delete this ?</p>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button
+        bsSize="large"
+        bsStyle="danger"
+        onClick={console.log('delete')}
+      >
+        Delete
+      </Button>
+      <Button
+        bsSize="large"
+        bsStyle="success"
+        onClick={props.cancel}
+      >
+        Cancel
+      </Button>
+    </Modal.Footer>
+  </Modal>
+);
+export default ConfirmDelete;
+
