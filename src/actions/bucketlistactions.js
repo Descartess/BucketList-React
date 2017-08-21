6f1478231918d3b5c4c9d20b0c7ea342b18b6312
Descartess <nyondopaul23@gmail.com>
2017-08-21 20:49:40 +0300

    Add delete, edit bucket list actions

--- src/actions/bucketlistactions.js
+++ src/actions/bucketlistactions.js
@@ -9,3 +9,24 @@ export const postBucketList = ({ name, completed_by }) => ({
   type: 'POST_BUCKETLISTS',
   payload: instance.post('/bucketlists', { name, completed_by }),
 });
+
+export const deleteBucketList = id => ({
+  type: 'DELETE_BUCKETLISTS',
+  payload: instance.delete(`/bucketlists/${id}`),
+});
+
+export const putBucketList = (id, { name, completed_by }) => ({
+  type: 'EDIT_BUCKETLISTS',
+  payload: instance.put(`/bucketlists/${id}`, { name, completed_by }),
+});
+
+export const selectBucketList = id => ({
+  type: 'SELECT_BUCKETLIST',
+  payload: id,
+});
+
+export const selectBucketItem = id => ({
+  type: 'SELECT_ITEM',
+  payload: id,
+});
+
